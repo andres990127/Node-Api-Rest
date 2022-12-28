af74c54c031963f5e9e16ccd63500253e0ac167f
Andrés Gutiérrez <andres_mau.gutierrez@uao.edu.co>
2022-12-28 21:10:42 +0000

    Implementando CRUD

---
+++ routes/products.router.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const faker = require('faker');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  const products = [];
+  const { size } = req.query;
+  const limit = size || 10;
+  for (let index = 0; index < limit; index++) {
+    products.push({
+      name: faker.commerce.productName(),
+      price: parseInt(faker.commerce.price(), 10),
+      image: faker.image.imageUrl(),
+    });
+  }
+  res.json(products);
+});
+
+router.get('/filter', (req, res) => {
+  res.send('Yo soy un filter');
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    id,
+    name: 'Product 2',
+    price: 2000
+  });
+});
+
+router.post('/', (req, res) => {
+  const body = req.body;
+  res.json({
+    message: 'Created',
+    data: body
+  })
+})
+
+router.patch('/:id', (req, res) => {
+  const id = req.params.id;
+  const body = req.body;
+  res.json({
+    message: 'Updated',
+    data: body,
+    id
+  })
+})
+
+router.delete('/:id', (req, res) => {
+  const id = req.params.id;
+  res.json({
+    message: 'Deleted',
+    id
+  })
+})
+
+module.exports = router;
+
